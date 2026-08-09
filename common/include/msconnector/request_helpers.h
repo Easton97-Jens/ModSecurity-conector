60cd43d0b396cf311dd33303eafbb12e2a69e7ea
Easton97-Jens <66330090+Easton97-Jens@users.noreply.github.com>
2026-08-09 17:25:21 +0200

    remove unsafe raw header getters

--- common/include/msconnector/request_helpers.h
+++ common/include/msconnector/request_helpers.h
@@ -9,8 +9,6 @@ void msconnector_request_init(msconnector_request *request);
 int msconnector_request_validate(const msconnector_request *request);
 int msconnector_request_validate_with_limits(const msconnector_request *request, const msconnector_resource_limits *limits);
 int msconnector_request_has_header(const msconnector_request *request, const char *name);
-const char *msconnector_request_header_value(const msconnector_request *request, const char *name);
-const char *msconnector_request_content_type(const msconnector_request *request);
 int msconnector_request_content_type_slice(const msconnector_request *request, const char **value, size_t *value_size);
 size_t msconnector_request_content_length(const msconnector_request *request, int *status);
 #ifdef __cplusplus
